1c858284d523b2b3d498c058aac68c84f00ca140
tiagopbop <tiagopbop@gmail.com>
2025-06-01 21:02:14 +0100

    first commit

---
+++ project/objects/MyWindow.js
@@ -0,0 +1,45 @@
+import {CGFobject} from "../../lib/CGF.js";
+
+/**
+ * MyWindow
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyWindow extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.initBuffers();
+    }
+
+    /**
+     * Initializes the WebGL buffers for the window
+     * Sets up the window vertices, indices, and texture coordinates
+     */
+    initBuffers() {
+        // Define vertices, indices, and other buffer data for the window
+        this.vertices = [
+            -1, -1, 0, // Bottom-left
+            1, -1, 0, // Bottom-right
+            -1, 1, 0, // Top-left
+            1, 1, 0, // Top-right
+        ];
+
+        this.indices = [0, 1, 2, 1, 3, 2];
+
+        this.texCoords = [0, 1, 1, 1, 0, 0, 1, 0];
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+
+    /**
+     * Displays the window
+     */
+    display() {
+        // Apply the window texture
+        this.scene.textureManager.windowMaterial.apply();
+
+        // Render the window
+        super.display();
+    }
+}
